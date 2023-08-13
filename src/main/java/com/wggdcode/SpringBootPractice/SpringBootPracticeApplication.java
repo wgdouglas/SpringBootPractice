0186935f26fffdf3fc0fa676e032d4b2fe27fd81
gfizzle <winstongdouglas4@gmail.com>
2023-08-13 18:32:31 -0500

    feature: Added constructor, toString method, getter & setter to student class. Also created a new student inside of main class

--- src/main/java/com/wggdcode/SpringBootPractice/SpringBootPracticeApplication.java
+++ src/main/java/com/wggdcode/SpringBootPractice/SpringBootPracticeApplication.java
@@ -4,7 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import student.Student;
 
+import java.time.LocalDate;
+import java.time.Month;
 import java.util.List;
 
 @SpringBootApplication
@@ -16,8 +19,16 @@ public class SpringBootPracticeApplication {
 	}
 
 	@GetMapping
-	public List<String> hello() {
-		return List.of("Hello", "World");
+	public List<Student> hello() {
+		return List.of(
+				new Student(
+						1L,
+						"Mariam",
+						"mariam.jamal@gmail.com",
+						LocalDate.of(2000, Month.JANUARY,5),
+						21
+				)
+		);
 	}
 }
 
