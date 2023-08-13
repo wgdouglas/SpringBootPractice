664c834e0220e5daba6645ff3e0e4228874e3eb0
gfizzle <winstongdouglas4@gmail.com>
2023-08-13 18:18:44 -0500

    feature: first commit with student class added

---
+++ src/main/java/student/Student.java
@@ -0,0 +1,11 @@
+package student;
+
+import java.time.LocalDate;
+
+public class Student {
+    public Long id;
+    public String name;
+    public String email;
+    public LocalDate dob;
+    public Integer age;
+}
---
+++ src/test/java/com/wggdcode/SpringBootPractice/SpringBootPracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wggdcode.SpringBootPractice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootPracticeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
