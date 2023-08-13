430b75a8f65cc7feb699a89b962ef4d21e54cc0f
gfizzle <winstongdouglas4@gmail.com>
2023-08-13 18:52:49 -0500

    feature: Added the API Layer with StudentController class

---
+++ src/main/java/student/StudentController.java
@@ -0,0 +1,26 @@
+package student;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalDate;
+import java.time.Month;
+import java.util.List;
+
+@RestController
+@RequestMapping(path = "api/version1/student")
+public class StudentController {
+    @GetMapping
+    public List<Student> hello() {
+        return List.of(
+                new Student(
+                        1L,
+                        "Mariam",
+                        "mariam.jamal@gmail.com",
+                        LocalDate.of(2000, Month.JANUARY, 5),
+                        21
+                )
+        );
+    }
+}
