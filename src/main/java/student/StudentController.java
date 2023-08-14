c4e3319c77fb585504c72402d896c2d311844980
gfizzle <winstongdouglas4@gmail.com>
2023-08-13 19:11:21 -0500

    feature: Added the Service Layer with StudentService class

--- src/main/java/student/StudentController.java
+++ src/main/java/student/StudentController.java
@@ -4,23 +4,19 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.time.LocalDate;
-import java.time.Month;
 import java.util.List;
 
 @RestController
 @RequestMapping(path = "api/version1/student")
 public class StudentController {
+    private final StudentService studentService;
+
+    public StudentController(StudentService studentService) {
+        this.studentService = studentService;
+    }
+
     @GetMapping
-    public List<Student> hello() {
-        return List.of(
-                new Student(
-                        1L,
-                        "Mariam",
-                        "mariam.jamal@gmail.com",
-                        LocalDate.of(2000, Month.JANUARY, 5),
-                        21
-                )
-        );
+    public List<Student> getStudents() {
+        return studentService.getStudents();
     }
 }
